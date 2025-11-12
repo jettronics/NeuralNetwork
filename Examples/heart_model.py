d0d3a1cb6c191afdb2aa4528a163c0dcf7aa1482
Thielemann Jens (VM-EPS/PJ-HWP-PM VM-EPS/PJ-HWP-PC) <Jens.Thielemann@bosch.com>
2025-11-12 08:27:26 +0100

    Heart model data normalization

--- Examples/heart_model.py
+++ Examples/heart_model.py
@@ -1,18 +1,38 @@
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
+from torch.utils.data import Dataset
 
 # Tutorial:
 # https://machinelearningmastery.com/develop-your-first-neural-network-with-pytorch-step-by-step/
 
-# load the dataset, split into input (X) and output (y) variables
-dataset = np.loadtxt('cleaned_merged_heart_dataset.csv', skiprows=1, delimiter=',')
-X = dataset[:,0:13] # every row, starting from 0, 13 values
-y = dataset[:,13] # every row, 13th value starting from 0
 
-# PyTorch is working with 32Bit
-X = torch.tensor(X, dtype=torch.float32)
-y = torch.tensor(y, dtype=torch.float32).reshape(-1, 1)
+class CSVDataset(Dataset):
+    def __init__(self, csv_file, norm=True):
+        # load the dataset, split into input (X) and output (y) variables
+        dataset = np.loadtxt(csv_file, skiprows=1, delimiter=',')
+        X = dataset[:,0:13] # every row, starting from 0, 13 values
+        y = dataset[:,13] # every row, 13th value starting from 0
+
+        # PyTorch is working with 32Bit
+        self.X = torch.tensor(X, dtype=torch.float32)
+        self.y = torch.tensor(y, dtype=torch.float32).reshape(-1, 1)
+
+        # Wenn train=True: berechne Normalisierung
+        if norm:
+            self.mean = self.X.mean(dim=0)
+            self.std = self.X.std(dim=0)
+            self.X = (self.X - self.mean) / self.std
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+   
+
+train_csv = "cleaned_merged_heart_dataset.csv";
+train_data = CSVDataset(train_csv, norm=True)
 
 model = torch.nn.Sequential(
     torch.nn.Linear(13, 25),
@@ -33,9 +53,9 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) # alternative
 
 for epoch in range(epochs):
     for i in range(150, 1150, batch):  # len(X)
-        Xbatch = X[i:i+batch]
+        Xbatch = train_data.X[i:i+batch]
         y_pred = model(Xbatch)
-        ybatch = y[i:i+batch]
+        ybatch = train_data.y[i:i+batch]
         loss = loss_fn(y_pred, ybatch)
         losses.append(loss.item())
         optimizer.zero_grad()
@@ -45,8 +65,8 @@ for epoch in range(epochs):
         
 # compute accuracy (no_grad is optional)
 with torch.no_grad():
-    y_pred = model(X)
-    accuracy = (y_pred.round() == y).float().mean()
+    y_pred = model(train_data.X)
+    accuracy = (y_pred.round() == train_data.y).float().mean()
     print(f"Accuracy {accuracy}")
 
 plt.figure(figsize=(10,7))
@@ -54,9 +74,9 @@ plt.plot(losses)
 plt.xlabel("Epochs",fontsize=22)
 plt.ylabel("Loss",fontsize=22)
 
-#plt.savefig('C:\GitHub\\NeuralNetwork\Images\Heart_Disease_Prediction_PyTorch.jpg', format='jpg', dpi=200)
+plt.savefig('C:\GitHub\\NeuralNetwork\Images\Heart_Disease_Prediction_PyTorch_Norm.jpg', format='jpg', dpi=200)
 plt.show()
 
-predictions = (model(X) > 0.5).int()
+predictions = (model(train_data.X) > 0.5).int()
 for i in range(420,470):
-    print('%s => %d (expected %d)' % (X[i].tolist(), predictions[i], y[i]))
+    print('%s => %d (expected %d)' % (train_data.X[i].tolist(), predictions[i], train_data.y[i]))
